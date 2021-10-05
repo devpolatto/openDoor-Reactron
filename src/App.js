97e6b77d3aeb3438a18d92419d7cf89c21c7386f
Angelo Polatto <polatto.xcode.job@gmail.com>
2021-10-05 18:46:02 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
+import { useState } from 'react';
+
+import api from './services/api'
+
+const host = "http://192.168.1.50"
+const user = process.env.USER_IDFLEX
+const pass = process.env.PASS_IDFLEX
+
 function App() {
+
+  const [session, setSession] = useState('')
+
+  async function OpenDor (session){
+      await api.post(host + '/execute_actions.fcgi?session=' + session, {
+          actions: [ { action: "sec_box", parameters: "id=65793, reason=3" } ]
+      })
+  }
+
+  async function requestSession(){
+      await api.post(host + '/login.fcgi',{
+          login: user,
+          password: pass
+      })
+      .then((response) => {
+          console.log(response.data.session)  
+          OpenDor(response.data.session)
+          setSession(response.data.session)
+
+      })
+      .catch(erro => console.log(erro))
+  }
+
   return (
     <div className="App">
       <h2>Open Door</h2>
-      <h2>Open Door</h2>
-      <button>Open</button>
+      <h2>Session: {session}</h2>
+      <button onClick={requestSession}>Open</button>
     </div>
   );
 }
